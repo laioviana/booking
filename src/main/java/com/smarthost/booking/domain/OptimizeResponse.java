5ca68be6b644b2eb21923b8e97297bef249c4413
Laio Dias <laioviana@gmail.com>
2021-03-23 20:30:58 -0300

    Adding lombok builder to response class

--- src/main/java/com/smarthost/booking/domain/OptimizeResponse.java
+++ src/main/java/com/smarthost/booking/domain/OptimizeResponse.java
@@ -1,10 +1,15 @@
 package com.smarthost.booking.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class OptimizeResponse {
-
+    Integer usageEconomy;
+    Integer amountPaidEconomy;
+    Integer usagePremium;
+    Integer amountPaidPremium;
 }
