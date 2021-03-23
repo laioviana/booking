ee7295abfcf9156c82c70d3afd9a2222d66f2fb1
Laio Dias <laioviana@gmail.com>
2021-03-23 20:35:22 -0300

    Controller calling optimize service

--- src/main/java/com/smarthost/booking/controller/RoomController.java
+++ src/main/java/com/smarthost/booking/controller/RoomController.java
@@ -1,17 +1,16 @@
 package com.smarthost.booking.controller;
 
-import com.smarthost.booking.domain.Room;
+import com.smarthost.booking.domain.OptimizeRequest;
+import com.smarthost.booking.domain.OptimizeResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
 @Api("Room Controller")
 public interface RoomController {
-    @ApiOperation(value = "Optimize occupancy", response = Room[].class)
+    @ApiOperation(value = "Optimize occupancy", response = OptimizeResponse[].class)
     @ApiResponses(value = {
             @ApiResponse(code= 200, message = "Successful"),
             @ApiResponse(code= 401, message = "Not Authorized"),
@@ -21,5 +20,5 @@ public interface RoomController {
             dataType = "string",
             paramType = "header",
             required = true) })*/
-    ResponseEntity<List<Room>> optimizeRooms(Integer roomAvailableEconomy, Integer roomAvailablePremium);
+    ResponseEntity<OptimizeResponse> optimizeRooms(OptimizeRequest optimizeRequest);
 }
--- src/main/java/com/smarthost/booking/controller/impl/RoomControllerImpl.java
+++ src/main/java/com/smarthost/booking/controller/impl/RoomControllerImpl.java
@@ -1,25 +1,26 @@
 package com.smarthost.booking.controller.impl;
 
 import com.smarthost.booking.controller.RoomController;
-import com.smarthost.booking.domain.Room;
+import com.smarthost.booking.domain.OptimizeRequest;
+import com.smarthost.booking.domain.OptimizeResponse;
+import com.smarthost.booking.service.RoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/room")
 public class RoomControllerImpl implements RoomController {
 
-    @GetMapping("/optimize-rooms/{roomAvailableEconomy}/{roomAvailablePremium}")
-    public ResponseEntity<List<Room>> optimizeRooms(@PathVariable Integer roomAvailableEconomy, @PathVariable Integer roomAvailablePremium) {
-        return ResponseEntity.ok(new ArrayList<>());
+    private RoomService roomService;
+
+    @PostMapping("/optimize-rooms")
+    public ResponseEntity<OptimizeResponse> optimizeRooms(@RequestBody OptimizeRequest optimizeRequest) {
+        return ResponseEntity.ok(roomService.optimizeRooms(optimizeRequest));
     }
 
 }
