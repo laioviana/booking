270734115bc6943d27d9245b8fba2684ccf1cdca
Laio Dias <laioviana@gmail.com>
2021-03-23 20:14:17 -0300

    Adding service layer

---
+++ src/main/java/com/smarthost/booking/service/RoomService.java
@@ -0,0 +1,14 @@
+package com.smarthost.booking.service;
+
+import com.smarthost.booking.domain.OptimizeRequest;
+import com.smarthost.booking.domain.OptimizeResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoomService {
+
+    public OptimizeResponse optimizeRooms(OptimizeRequest optimizeRequest) {
+        return null;
+    }
+
+}
